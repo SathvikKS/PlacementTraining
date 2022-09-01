eeddca4ecfd8f6b11bc67983683a3f008d2fd1b6
Sathvik K S <sathvikks8@gmail.com>
2022-09-01 15:45:11 +0530

    HashMapExample

---
+++ src/HashMapExample.java
@@ -0,0 +1,13 @@
+import java.util.TreeMap;
+
+public class HashMapExample {
+    public static void main(String[] args) {
+//        HashMap<String, String> map = new HashMap<>();
+//        LinkedHashMap<String, String> map = new LinkedHashMap<>();
+        TreeMap<String, String> map = new TreeMap<>();
+        map.put("Zara", "8");
+        map.put("Mahnaz", "31");
+        map.put("Sks", "21");
+        System.out.println(map);
+    }
+}
