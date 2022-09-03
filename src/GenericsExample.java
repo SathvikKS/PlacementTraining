9cdbda23657c24d126798087853c0e21fb978337
Sathvik K S <sathvikks8@gmail.com>
2022-09-03 14:58:03 +0530

    Generics example

---
+++ src/GenericsExample.java
@@ -0,0 +1,16 @@
+public class GenericsExample {
+    public static <E> void printArray(E[] elements) {
+        for (E element: elements)
+            System.out.println(element);
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Integer[] intArray = {10, 20, 30, 40, 50};
+        Character[] charArray = {'a', 'b', 'c', 'd', 'e'};
+        System.out.println("Printing Integer Array");;
+        printArray(intArray);
+        System.out.println("Printing Character Array");
+        printArray(charArray);
+    }
+}
