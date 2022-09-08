36453536cbeb6bc8d66f7f06c591dd8ca323e8e3
Sathvik K S <sathvikks8@gmail.com>
2022-09-08 11:48:40 +0530

    updates

--- src/Demo.java
+++
@@ -1,9 +0,0 @@
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-
-public class Demo {
-    public static void main(String[] args) {
-        int[] a = {1,2,3,4};
-        System.out.println();
-    }
-}
